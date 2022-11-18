e60a2bb24d9aa505ffa95d810687478c49e27988
Uckermark <93996544+Uckermark@users.noreply.github.com>
2022-11-18 12:57:13 +0100

    Update Weedra1nController.swift

--- weedra1n/Weedra1nController.swift
+++ weedra1n/Weedra1nController.swift
@@ -276,13 +276,13 @@ public class Actions: ObservableObject {
         }
         addToLog(msg: "[*] Downloading update")
         DispatchQueue.global(qos: .utility).async {
-            let dArgs: String
+            let dArgs: [String]
             if UseDev {
-                dArgs = "-u --dev"
+                dArgs = ["-u", "--dev"]
             } else {
-                dArgs = "-u"
+                dArgs = ["-u"]
             }
-            let ret = spawn(command: helper, args: [dArgs], root: true)
+            let ret = spawn(command: helper, args: dArgs, root: true)
             DispatchQueue.main.async {
                 self.vLog(msg: ret.1)
                 DispatchQueue.global(qos: .utility).async {
