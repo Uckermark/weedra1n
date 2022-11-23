5cc4b3b7c6fe5c1d3d6b3e353f00d9392c920619
Uckermark <llausen@protonmail.com>
2022-11-23 21:05:09 +0100

    sudo should be upgraded for proper rootless support though

--- weedra1n/Required/patch.sh
+++ weedra1n/Required/patch.sh
@@ -2,4 +2,4 @@
 
 printf "Types: deb\nURIs: https://mineek.github.io/repo/\nSuites: ./\nComponents:\n\nTypes: deb\nURIs: https://repo.palera.in/\nSuites: ./\nComponents:\n" > /var/jb/etc/apt/sources.list.d/procursus.sources
 apt update --allow-insecure-repositories
-apt install libiosexec1 --allow-unauthenticated -y
+apt install libiosexec1 sudo --allow-unauthenticated -y
