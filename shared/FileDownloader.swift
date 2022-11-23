dc7fd6b3e028f9db1d625205f88052620155d6c8
Uckermark <llausen@protonmail.com>
2022-11-23 13:54:06 +0100

    clean helper and fix download of bootstrap

---
+++ shared/FileDownloader.swift
@@ -0,0 +1,42 @@
+//
+//  FileDownloader.swift
+//  weedra1n
+//
+//  Created by Leonard Lausen on 23.11.22.
+//
+
+import Foundation
+
+class FileDownloader {
+    static func loadFileSync(url: URL, completion: @escaping (String?, Error?) -> Void)
+    {
+        let documentsUrl = URL(string: "file:///var/mobile/Documents/weedra1n/")!
+        
+        let destinationUrl = documentsUrl.appendingPathComponent(url.lastPathComponent)
+        
+        if FileManager().fileExists(atPath: destinationUrl.path)
+        {
+            print("File already exists [\(destinationUrl.path)]")
+            completion(destinationUrl.path, nil)
+        }
+        else if let dataFromURL = NSData(contentsOf: url)
+        {
+            if dataFromURL.write(to: destinationUrl, atomically: true)
+            {
+                print("file saved [\(destinationUrl.path)]")
+                completion(destinationUrl.path, nil)
+            }
+            else
+            {
+                print("error saving file")
+                let error = NSError(domain:"Error saving file", code:1001, userInfo:nil)
+                completion(destinationUrl.path, error)
+            }
+        }
+        else
+        {
+            let error = NSError(domain:"Error downloading file", code:1002, userInfo:nil)
+            completion(destinationUrl.path, error)
+        }
+    }
+}
--- weedra1nHelper/main.swift
+++ weedra1nHelper/main.swift
@@ -198,38 +198,4 @@ struct Strap: ParsableCommand {
     }
 }
 
-class FileDownloader {
-    static func loadFileSync(url: URL, completion: @escaping (String?, Error?) -> Void)
-    {
-        let documentsUrl = URL(string: "file:///var/mobile/Documents/weedra1n/")!
-        
-        let destinationUrl = documentsUrl.appendingPathComponent(url.lastPathComponent)
-        
-        if FileManager().fileExists(atPath: destinationUrl.path)
-        {
-            print("File already exists [\(destinationUrl.path)]")
-            completion(destinationUrl.path, nil)
-        }
-        else if let dataFromURL = NSData(contentsOf: url)
-        {
-            if dataFromURL.write(to: destinationUrl, atomically: true)
-            {
-                print("file saved [\(destinationUrl.path)]")
-                completion(destinationUrl.path, nil)
-            }
-            else
-            {
-                print("error saving file")
-                let error = NSError(domain:"Error saving file", code:1001, userInfo:nil)
-                completion(destinationUrl.path, error)
-            }
-        }
-        else
-        {
-            let error = NSError(domain:"Error downloading file", code:1002, userInfo:nil)
-            completion(destinationUrl.path, error)
-        }
-    }
-}
-
 Strap.main()
